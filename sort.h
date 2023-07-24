883e261ffa29522d114848cf5ff7fcdeb79dc13f
chukwucyprian <chukwucyprian262@gmail.com>
2023-07-24 19:09:59 +0000

    correction

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -14,6 +15,12 @@ const int n;
 struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
+
+typedef enum bool
+{
+false = 0,
+true
+} bool;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
